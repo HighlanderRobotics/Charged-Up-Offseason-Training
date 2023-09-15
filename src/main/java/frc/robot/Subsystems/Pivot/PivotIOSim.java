542e547ce41c3d20a56baa38f668bfb72c03360e
RobotiX <robotics@piedmont.local>
2023-09-14 17:28:15 -0700

    pivot subsystem

---
+++ src/main/java/frc/robot/Subsystems/Pivot/PivotIOSim.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.Pivot;
+
+import com.ctre.phoenixpro.hardware.TalonFX;
+
+/** Add your docs here. */
+public class PivotIOSim implements PivotIO{
+    private final TalonFX motor = new TalonFX(0);
+
+
+
+    public PivotIOInputsAutoLogged updateInputs() {
+        PivotIOInputsAutoLogged input = new PivotIOInputsAutoLogged();
+
+        //var motorSimState = motor.getSimState();
+
+        input.motorOutputVolts = motor.getSupplyVoltage().getValue();
+        input.velocityRPM = motor.getVelocity().getValue()/60;
+        input.currentDrawAmps = motor.getStatorCurrent().getValue();
+        input.temperatureCelsius = 0;
+        input.positionDegrees = motor.getPosition().getValue();
+
+        return input;
+
+    }
+
+    @Override
+    public void setPosition() {
+        //Set motor position
+    }
+    
+}
---
+++ src/main/java/frc/robot/Subsystems/Pivot/PivotSubsystem.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.Pivot;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public class PivotSubsystem extends SubsystemBase {
+    PivotIO io;
+    PivotIOInputsAutoLogged inputs;
+
+    public PivotSubsystem(PivotIO io) {
+        this.io = io;
+        inputs =  new PivotIOInputsAutoLogged();
+    }
+
+    public CommandBase run(double volts) {
+        return new RunCommand(() -> {
+            io.setPosition();
+        }, this);
+    }   
+
+    @Override
+    public void periodic() {
+        inputs = io.updateInputs();
+        Logger.getInstance().processInputs("Intake", inputs);
+    }
+
+
+
+}
