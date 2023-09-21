269d6f16c669be9e6e46b6272f6dbafb9898dfa8
RobotiX <robotics@piedmont.local>
2023-09-21 16:55:28 -0700

    pivot

--- src/main/java/frc/robot/Subsystems/Pivot/PivotIOSim.java
+++ src/main/java/frc/robot/Subsystems/Pivot/PivotIOSim.java
@@ -6,21 +6,31 @@ package frc.robot.Subsystems.Pivot;
 
 import com.ctre.phoenixpro.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
 /** Add your docs here. */
-public class PivotIOSim implements PivotIO{
+public class PivotIOSim implements PivotIO {
     private final TalonFX motor = new TalonFX(0);
+    SingleJointedArmSim arm = new SingleJointedArmSim(DCMotor.getFalcon500(1), 27 * (24 / 18),
+            SingleJointedArmSim.estimateMOI(Units.inchesToMeters(30), 3), Units.inchesToMeters(30), 0, 3.5, true);
+    PIDController pid = new PIDController(1, 0, 0);
 
     public PivotIOInputsAutoLogged updateInputs() {
 
         PivotIOInputsAutoLogged input = new PivotIOInputsAutoLogged();
 
-        //var motorSimState = motor.getSimState();
+        // var motorSimState = motor.getSimState();
 
-        input.velocityRPM = motor.getVelocity().getValue()*60;
-        input.currentDrawAmps = motor.getStatorCurrent().getValue();
+        input.velocityRPM = arm.getVelocityRadPerSec() * 60;
+        input.currentDrawAmps = arm.getCurrentDrawAmps();
         input.temperatureCelsius = 0;
-        input.motorOutputVolts = motor.getSupplyVoltage().getValue();
-        input.positionDegrees = motor.getPosition().getValue();
+        input.motorOutputVolts = arm.getOutput(0);
+        input.positionDegrees = Units.radiansToDegrees(arm.getAngleRads());
+
+        arm.update(0.020);
 
         return input;
 
@@ -28,7 +38,8 @@ public class PivotIOSim implements PivotIO{
 
     @Override
     public void setPosition(double degrees) {
-        //Set motor position
+        double outputVolts = pid.calculate(Units.radiansToDegrees(arm.getAngleRads()), degrees);
+        arm.setInput(outputVolts);
     }
-    
+
 }
